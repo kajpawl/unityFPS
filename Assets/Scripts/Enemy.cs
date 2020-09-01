30fa6cb92574280f5aa8ff5977d642be2b491c5c
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-01 22:14:18 +0200

    Calculate enemy path

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Stats")]
+    public int curHp;
+    public int maxHp;
+    public int scoreToGive;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float attackRange;
+    public float yPathOffset;
+
+    private List<Vector3> path;
+
+    private Weapon weapon;
+    private GameObject target;
+
+    void Start()
+    {
+        // get the components
+        weapon = GetComponent<Weapon>();
+        target = FindObjectOfType<Player>().gameObject;
+
+        InvokeRepeating("UpdatePath", 0.0f, 0.5f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void UpdatePath()
+    {
+        // calculate a path to the target
+        NavMeshPath navMeshPath = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, navMeshPath);
+
+        // save that as a list
+        path = navMeshPath.corners.ToList();
+    }
+}
