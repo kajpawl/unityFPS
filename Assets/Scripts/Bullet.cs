f68fb4c488d5853726a46866e6db92e82949e17c
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-02 18:08:40 +0200

    Add particle effects

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,6 +8,8 @@ public class Bullet : MonoBehaviour
     public float lifetime;      // how long until the bullet despawns>
     private float shootTime;    // time the bullet was shot
 
+    public GameObject hitParticle;
+
     void OnEnable()
     {
         shootTime = Time.time;
@@ -27,6 +29,10 @@ public class Bullet : MonoBehaviour
             other.GetComponent<Player>().TakeDamage(damage);
         else if (other.CompareTag("Enemy"))
             other.GetComponent<Enemy>().TakeDamage(damage);
+
+        // create the hit particle
+        GameObject obj = Instantiate(hitParticle, transform.position, Quaternion.identity);
+        Destroy(obj, 0.6f);
 
         // disable the bullet
         gameObject.SetActive(false);
